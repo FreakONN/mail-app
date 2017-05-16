4f4fb5d4b880db3cf26db92927466579d69f56b5
marko <marko.bozic@protonmail.com>
2017-05-16 18:58:17 +0200

    added delete option

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -13,7 +13,6 @@ $scope.addUser = function(){
     name: $scope.newPost.name,
     username: $scope.newPost.username,
     email: $scope.newPost.email,
-
     address: 
     {
             street : $scope.newPost.address.street,
@@ -31,4 +30,28 @@ $scope.addUser = function(){
     console.log(error);
   });
 }
+
+$scope.removeUser = function(index)
+{
+    $scope.fiveDay.splice(index, 1);
+}
+  //ne radi kako treba - na stacku predlažu upgrade na 1.6.*
+  $scope.removePost = function () {
+            var data = 
+            {
+              id: $scope.newPost.id
+            } 
+            //Error: $scope.newPost is undefined
+            //možemo direktno proslijediti vrijdnost ili preko $routeParams i indexa dokhvatiti 'id'
+            $http.delete('http://jsonplaceholder.typicode.com/users/' + data)
+            .success(function (data, status, headers, config) {
+                $scope.ServerResponse = data;
+            })
+              .error(function (data, status, header) {
+                $scope.ServerResponse = htmlDecode("Data: " + data +
+                    "\n\n\n\nstatus: " + status +
+                    "\n\n\n\nheaders: " + header +
+                    "\n\n\n\nconfig: " + config);
+            });
+        };
 }]);
